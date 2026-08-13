229fd986a36dc2af9824fa39a1a63dcef40a9f68
hamzamallick <mohdhamza1928@gmail.com>
2026-08-14 02:29:23 +0530

    new

--- app/src/main/java/com/techfinder/localserviceprovider/presentation/homescreen/HomeScreen.kt
+++ app/src/main/java/com/techfinder/localserviceprovider/presentation/homescreen/HomeScreen.kt
@@ -36,7 +36,7 @@ fun HomeScreen(){
         )
         Spacer(modifier = Modifier.height(20.dp))
         Text(
-            text = "Welcome Hamza",
+            text = "Hamza aa gaya",
             fontSize = 45.sp,
             color = Color.Black
         )
